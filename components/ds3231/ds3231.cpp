47e74c0a060f66f7422e1abf7b12aeaf624ae9a1
Nerdiy.de <accounts@Nerdiy.de>
2026-04-08 14:45:33 +0200

    Remove timezone logging from DS3231 component configuration

--- components/ds3231/ds3231.cpp
+++ components/ds3231/ds3231.cpp
@@ -24,8 +24,6 @@ void DS3231Component::dump_config() {
   if (this->is_failed()) {
     ESP_LOGE(TAG, "Communication with DS3231 failed!");
   }
-  const std::string timezone = this->get_timezone();
-  ESP_LOGCONFIG(TAG, "  Timezone: '%s'", timezone.c_str());
 }
 
 float DS3231Component::get_setup_priority() const { return setup_priority::DATA; }
